87dd10e53430c16b75d1a3c98677087bf493dfd7
unknown <c.w.lun96@gmail.com>
2019-03-30 15:58:37 +0800

    first commit

---
+++ app/Http/Controllers/choyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class choyController extends Controller
+{
+
+    public function wingLun()
+    {
+        return view('choy');
+    }
+}
